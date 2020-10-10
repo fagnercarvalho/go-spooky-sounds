3357c2c58e7e45c5a63044a9b7c2d2393a23d1fc
Fagner Carvalho <fagnerncarvalho@gmail.com>
2020-10-10 17:30:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# go-spooky-sounds
+
+[![GoDoc](https://godoc.org/github.com/fagnercarvalho/go-spooky-sounds?status.svg)](https://godoc.org/github.com/fagnercarvalho/go-spooky-sounds) ![Go version](https://img.shields.io/github/go-mod/go-version/fagnercarvalho/go-spooky-sounds)
+
+Play spooky sound effects for Halloween from your speaker or Bluetooth speaker.
+
+Made in Go.
+
+### Requirements
+
+- This utility was tested in Go 1.13.4.
+- This only supports Linux.
+- If you want to play these spooky sounds in your Bluetooth speaker you will need to pair your system with the Bluetooth device first. I made a brief guide on [how to pair and connect to a Bluetooth device here]().
+
+### Installation
+
+```
+go install github.com/fagnercarvalho/go-spooky-sounds
+```
+
+### Usage
+
+This will play a spooky sound periodically from a list of 6 predefined sounds. This can be used in your Halloween Party or just to get you in the mood of the season.
+
+You can add your own files in the sounds folder. They must be in the .wav format and have a sample rate of 44100 Hz (most tracks in .wav do, please use ffmpeg or libopus to check the rate or do a rate conversion to 44100 Hz).
+
+### Examples
+
+Here I played the spooky sounds directly in my Raspberry Pi audio jack.
+
+```
+go-spooky-sounds.exe -device "hw:CARD=Headphones,DEV=0"
+```
+
+Or you can play to your Bluetooth device by using the [bluez-alsa library](https://github.com/Arkq/bluez-alsa) virtual PCM.
+
+```
+go-spooky-sounds.exe -device "bluealsa:DEV=78:44:05:EB:93:71,PROFILE=a2dp"
+```
+
+The default value for the maximum interval between the sounds is 15 minutes but you can change this value by using the `maximumInterval` option.
+
+```
+go-spooky-sounds.exe -device "bluealsa:DEV=78:44:05:EB:93:71,PROFILE=a2dp" -maximumInterval 10
+```
+
+### How it works?
+
+Periodically this will read the sound effect (a .wav file) to be played and then send the byte array of sound samples to the PCM device (pulse code modulation) by using the ALSA API (Advanced Linux Sound Architecture) made in C. The ALSA API will then call the driver that will be able to call the actual hardware device to play the sound. Specifically for Bluetooth devices the [bluez-alsa library](https://github.com/Arkq/bluez-alsa) must be used to create a virtual PCM that will expose the Bluetooth device as any other sound device available in the system.
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"time"
+
+	asla "github.com/cocoonlife/goalsa"
+	"github.com/youpy/go-wav"
+)
+
+var lastSpookySound string = ""
+
+func main() {
+	// get the devices in your system by using the `aplay -L` command
+	// Example of a headphone jack input in raspberrypi
+	// 	- hw:CARD=Headphones,DEV=0
+	// Example of a virtual PCM to a bluetooth speaker by using the bluealsa library
+	// - bluealsa:DEV=78:44:05:EB:93:71,PROFILE=a2dp
+	deviceName := flag.String("device", "bluealsa:DEV=78:44:05:EB:93:71,PROFILE=a2dp", "Device name where the spooky sounds will be played")
+	maximumInterval := flag.Int("maximumInterval", 15, "Maximum interval for when the spooky sound can be played")
+	flag.Parse()
+
+	// we want our spooky sounds to be unpredictable
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		// keep playing spooky sounds unless stopped
+		sound := nextSpookySound()
+		samples := readSpookySound(sound)
+		playSpookySound(*deviceName, samples)
+
+		// wait an unexpected amount of time for the next spooky sound
+		sleepTime := time.Duration(rand.Int31n(*maximumInterval)) * time.Second
+		fmt.Printf("Play next spooky sound on %s\n", sleepTime)
+		time.Sleep(sleepTime)
+	}
+}
+
+func nextSpookySound() string {
+	spookySounds := getSpookySounds()
+	candidate := spookySounds[rand.Intn(len(spookySounds))]
+
+	// never play the same spooky sound twice
+	if candidate == lastSpookySound {
+		return nextSpookySound()
+	} else {
+		lastSpookySound = candidate
+	}
+
+	return candidate
+}
+
+func getSpookySounds() []string {
+	return []string{"bell", "cat", "laugh", "hauntedhouse", "raven", "witches"}
+}
+
+func readSpookySound(path string) []int16 {
+	file, _ := os.Open("sounds/" + path + ".wav")
+	reader := wav.NewReader(file)
+
+	defer file.Close()
+
+	// http://soundfile.sapp.org/doc/WaveFormat/
+	format, err := reader.Format()
+	checkErr(err)
+
+	fmt.Printf("Reading %v.%v\n", path, "wav")
+	fmt.Printf("File info: %v\n", format)
+
+	var data []int16
+	for {
+		samples, err := reader.ReadSamples()
+		if err == io.EOF {
+			break
+		}
+
+		for _, sample := range samples {
+			// convert stereo to mono by only getting the first channel
+			value := int16(reader.IntValue(sample, 0))
+			data = append(data, value)
+		}
+	}
+
+	return data
+}
+
+func playSpookySound(deviceName string, data []int16) {
+	fmt.Printf("Playing sound on %v\n", deviceName)
+
+	// only supports 44100 Hz sample rate
+	// any other wav file must be converted to 44100 Hz for this to work
+	// you can use ffmpeg or libopus in any OS to do the rate conversion
+	p, err := asla.NewPlaybackDevice(deviceName, 1, asla.FormatS16LE, 44100,
+		asla.BufferParams{})
+	checkErr(err)
+
+	// send the data to the PCM (pulse code modulation) real or virtual device that will play the spooky sound
+	_, err = p.Write(data)
+	checkErr(err)
+
+	p.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
